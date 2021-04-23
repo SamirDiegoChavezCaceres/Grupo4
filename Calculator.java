09e46fd859336cdc661f69d18a482f2248d77103
AlexWilliams <aolaechea@unsa.edu.pe>
2021-04-23 11:02:10 -0500

    Se implemento la funcion add(adicion) y la funcion sub(sustraccion) a la calculadora

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,12 @@
 class Calculator {
-  int add(int a, int b){ return 0; }
-  int sub(int a, int b){ return 0; }
+  int add(int a, int b){
+	  int sum = a + b; 
+	  return suma;
+  }
+  int sub(int a, int b){ 
+	  int res = a - b;
+	  return ; 
+  }
   int mul(int a, int b){ return 0; }
   int div(int a, int b){ return 0; }
   int mod(int a, int b){ return 0; }
